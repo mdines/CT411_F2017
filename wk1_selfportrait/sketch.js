82ecbe85ba16bd2c999a3dd33d18d01d3e08ab84
Michelle Dines <31594177+mdines@users.noreply.github.com>
2017-09-05 19:23:18 -0400

    Initial upload, wk1 homework

---
+++ wk1_selfportrait/sketch.js
@@ -0,0 +1,58 @@
+function setup(){
+createCanvas(800,600);    
+    
+}
+function draw(){
+background(0,80,255); 
+
+//fill(0);
+//noStroke();
+//rect(100, 200, 100, 100);
+//noFill();
+////fill(255, 13, 100, 100);
+//stroke(255);
+//strokeWeight(10);
+//    ellipse(100, 200, 100, 100);
+
+//    noFill();
+//stroke(255, 102, 0);
+//curve(5, 26, 5, 26, 73, 24, 73, 61);
+//stroke(0);
+//curve(5, 26, 73, 24, 73, 61, 15, 65);
+//stroke(255, 102, 0);
+//curve(73, 24, 73, 61, 15, 65, 15, 65);
+
+fill(0); 
+noStroke();
+rect(285, 252, 230, 194.5); 
+
+fill(0); 
+noStroke();
+ellipse(400, 269.5, 232, 232);
+
+fill(214, 156, 109); 
+noStroke();
+ellipse(402, 261.5, 164, 164);   
+
+fill(0); 
+noStroke();
+ellipse(443.5, 198, 61, 61);
+
+fill(0); 
+noStroke();
+ellipse(477.5, 239, 61, 61);
+
+fill(0);
+noStroke();
+triangle(412.699, 252, 428.349, 236, 444, 252);
+    
+fill(0);
+noStroke();
+triangle(354.699, 252, 370.349, 236, 386, 252);
+    
+fill(255, 123, 112);
+noStroke();
+arc(400, 300, 45, 45, 0, PI+QUARTER_PI, CHORD);
+    
+
+} 
