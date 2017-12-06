0ad71d3ab538c800dc1a8dcc87906297d090b7f6
Michelle Dines <31594177+mdines@users.noreply.github.com>
2017-12-05 19:17:46 -0500

    final v2

--- InfiniteCanvas/sketch.js
+++ InfiniteCanvas/sketch.js
@@ -1,11 +1,21 @@
 //var img
 
 
+var rectX = 800;
+var rectY = 382;
+var rectSize = 20;
+var rectTop = rectY;
+var rectBottom = rectY + rectSize;
+var rectLeft = rectX;
+var rectRight = rectX + rectSize;
 
 var bubbles = [];
 
 var cols = 100;
 var rows = 100;
+
+var state = 1;
+
 
 
 //function preload(){
@@ -35,27 +45,50 @@ function mousePressed() {
     bubbles[i].clicked(mouseX, mouseY);
   }
 
+  if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && state ==1){
+      state = 2;
+  }
 
-}
+
+}  
+
+
 
 
 function draw() {
-  background(0);
+  
 
 //image(img, 0, 0);    
+   
     
+ if(state == 1){
+     background(0);
+     
+     fill(195, 0, 155);
+     textSize(25);
+//     textMode(CENTER);
+//     rectMode(CENTER);
+     rect(rectX, rectY, rectSize, rectSize);
+     text("welcome to infinite canvas!!", 400, height/2);
+     text("click the square creative", 530, 400);
+     rect(windowWidth, windowHeight);
+     
+ } else if (state == 2) {
+     background(0);
+   
     
-  for (var i = 0; i < cols; i++){
-        for (var j = 0; j < rows; j++) {
-               var x = i * 50;
-               var y = j * 50;
+      for (var i = 0; i < cols; i++){
+            for (var j = 0; j < rows; j++) {
+                   var x = i * 50;
+                   var y = j * 50;
 
-        }
-  }
+            }
+      }
     
-  for (var i = 0; i < bubbles.length; i++) {
-    bubbles[i].show();
-  }
+      for (var i = 0; i < bubbles.length; i++) {
+        bubbles[i].show();
+      }
+    }
 }
 
 class Bubble {
